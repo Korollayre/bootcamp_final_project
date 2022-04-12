eec25baf4eae2574d302605dd5e5a06035c49201
Korollayre <mbuinichienko@mail.ru>
2022-04-12 21:24:41 +0300

    Add logic to work with filters

--- microservices/book_microservice/books_backend/application/services.py
+++ microservices/book_microservice/books_backend/application/services.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from typing import (
     List,
-    Optional,
+    Optional, Union,
 )
 
 from evraz.classic.app import (
@@ -16,12 +16,13 @@ from evraz.classic.messaging import (
 )
 from pydantic import validate_arguments
 
-# from .errors import (
-#     NoBook,
-#     BookExists,
-#     NoRights,
-#     BookedBook,
-# )
+from .errors import (
+    # NoBook,
+    # BookExists,
+    # NoRights,
+    # BookedBook,
+    FilterKeyError,
+)
 from .entities import Books
 
 from . import interfaces
@@ -51,29 +52,98 @@ class BooksInfo(DTO):
 @component
 class BooksManager:
     books_repo: interfaces.BooksRepo
+    user_publisher: Publisher
 
     @join_point
     @validate_arguments
-    def parse_broker_message(self, api: str, action: str, data: dict):
-        data['price'] = float(data.get('price')[1:])
-        book_info = BooksInfo(**data)
-        new_book = book_info.create_obj(Books)
+    def parse_broker_message(self, api: str, action: str, data: Union[dict, tuple]):
+        if action == 'create':
 
-        self.books_repo.add_instance(new_book)
+            data['price'] = float(data.get('price')[1:])
+            book_info = BooksInfo(**data)
+            new_book = book_info.create_obj(Books)
 
-    # @join_point
-    # @validate_arguments
-    # def get_book(self, book_id: int) -> Books:
-    #     book = self.books_repo.get_by_id(book_id)
-    #
-    #     if book is None:
-    #         raise NoBook(id=book_id)
-    #
-    #     return book
+            self.books_repo.add_instance(new_book)
+
+        elif action == 'send':
+            mail_data = {}
+
+            for tag in data:
+
+                mail_data[f'{tag}'] = None
+                pass
 
     @join_point
-    def get_books(self) -> List[Books]:
-        return self.books_repo.get_all()
+    @validate_arguments
+    def get_book(self, book_id: int) -> Books:
+        book = self.books_repo.get_by_id(book_id)
+
+        if book is None:
+            raise NoBook(id=book_id)
+
+        return book
+
+    @join_point
+    def get_books(self, query: dict) -> List[Books]:
+        books = self.filter_books(query)
+
+        return books
+
+    @join_point
+    @validate_arguments
+    def filter_books(self, query: dict) -> Optional[List[Books]]:
+        filter_query = []
+
+        header_filter = query.get('title')
+        if header_filter is not None:
+            flag, value = self.parse_filter_values(header_filter)
+            filter_query.append(self.filter_by_text_filters('title', flag, value))
+
+        header_filter = query.get('authors')
+        if header_filter is not None:
+            flag, value = self.parse_filter_values(header_filter)
+            filter_query.append(self.filter_by_text_filters('authors', flag, value))
+
+        text_filter = query.get('publisher')
+        if text_filter is not None:
+            flag, value = self.parse_filter_values(text_filter)
+            filter_query.append(self.filter_by_text_filters('publisher', flag, value))
+
+        likes_filters = query.get('price')
+        if likes_filters is not None:
+            flag, value = self.parse_filter_values(likes_filters)
+            filter_query.append(self.filter_by_numbers_filters('price', flag, value))
+
+        order_by_field = query.get('order_by')
+        if order_by_field in ('price', 'pages', None):
+            books = self.books_repo.get_filtered_books(filter_query, order_by_field)
+        else:
+            raise FilterKeyError()
+
+        limit = query.get('limit')
+        offset = query.get('offset')
+
+        return books[offset: limit + offset]
+
+    @staticmethod
+    def parse_filter_values(filter_value: str) -> List[str]:
+        return filter_value.split(':')
+
+    def filter_by_text_filters(self, field_name: str, filter_flag: str, filter_value: str):
+        filter_query = self.books_repo.get_by_text_filter(field_name, filter_flag, filter_value)
+
+        if filter_query is None:
+            raise FilterKeyError()
+
+        return filter_query
+
+    def filter_by_numbers_filters(self, field_name: str, filter_flag: str, filter_value: str):
+        filter_query = self.books_repo.get_by_numbers_filter(field_name, filter_flag, filter_value)
+
+        if filter_query is None:
+            raise FilterKeyError()
+
+        return filter_query
 
     # @join_point
     # @validate_arguments
