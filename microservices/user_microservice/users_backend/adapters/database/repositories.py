ebd40f047e28bd5e6c38eddc39090789015f0dde
Korollayre <mbuinichienko@mail.ru>
2022-04-13 10:43:23 +0300

    Some refactors

--- microservices/user_microservice/users_backend/adapters/database/repositories.py
+++ microservices/user_microservice/users_backend/adapters/database/repositories.py
@@ -1,6 +1,7 @@
-from typing import Optional
-
-from sqlalchemy import select
+from typing import (
+    Optional,
+    List,
+)
 
 from evraz.classic.components import component
 from evraz.classic.sql_storage import BaseRepository
@@ -11,18 +12,18 @@ from users_backend.application.entities import Users
 
 @component
 class UsersRepo(BaseRepository, interfaces.UsersRepo):
+    def get_all(self) -> List[Users]:
+        return self.session.query(Users).all()
+
     def get_by_id(self, user_id: int) -> Optional[Users]:
-        query = select(Users).where(Users.id == user_id)
-        return self.session.execute(query).scalars().one_or_none()
+        return self.session.query(Users).filter(Users.id == user_id).one_or_none()
 
     def get_by_email(self, email: str) -> Optional[Users]:
-        query = select(Users).where(Users.email == email)
-        return self.session.execute(query).scalars().one_or_none()
+        return self.session.query(Users).filter(Users.email == email).one_or_none()
 
     def add_instance(self, user: Users):
         self.session.add(user)
         self.session.flush()
 
-    def login(self, user_mail: str, user_password: str) -> Users:
-        query = select(Users).where(Users.email == user_mail, Users.password == user_password)
-        return self.session.execute(query).scalars().one_or_none()
+    def login(self, user_mail: str, user_password: str) -> Optional[Users]:
+        return self.session.query(Users).filter(Users.email == user_mail, Users.password == user_password).one_or_none()
