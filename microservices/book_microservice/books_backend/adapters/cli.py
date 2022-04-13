bea7dfcc934bf23f770ee6b1a120064b36f699e9
Korollayre <mbuinichienko@mail.ru>
2022-04-13 10:42:38 +0300

    Add logic for add timestamp for new books

--- microservices/book_microservice/books_backend/adapters/cli.py
+++ microservices/book_microservice/books_backend/adapters/cli.py
@@ -1,3 +1,4 @@
+import datetime
 import threading
 
 import click
@@ -13,7 +14,7 @@ join_point = join_points.join_point
 
 
 @join_point
-def send_request(tag: str, publisher: Publisher, barrier):
+def send_request(tag: str, publisher: Publisher, barrier, time: datetime):
     books_ids = []
 
     response = requests.get(f'https://api.itbook.store/1.0/search/{tag}').json()
@@ -30,6 +31,8 @@ def send_request(tag: str, publisher: Publisher, barrier):
 
     for book_id in books_ids:
         response = requests.get(f'https://api.itbook.store/1.0/books/{book_id}').json()
+
+        response['created_date'] = time
 
         publisher.publish(
             Message(
@@ -54,8 +57,10 @@ def create_cli(publisher, MessageBus):
     @click.argument('tags', nargs=-1, type=click.UNPROCESSED)
     def init(tags):
         barrier = threading.Barrier(len(tags) + 1)
+        time = datetime.datetime.now()
+
         for tag in tags:
-            thread = Thread(target=send_request, args=(tag, publisher, barrier), daemon=False)
+            thread = Thread(target=send_request, args=(tag, publisher, barrier, time), daemon=False)
             thread.start()
 
         barrier.wait()
@@ -66,7 +71,10 @@ def create_cli(publisher, MessageBus):
                 {
                     'api': 'books',
                     'action': 'send',
-                    'data': tags,
+                    'data': {
+                        'tags': tags,
+                        'time': time,
+                    },
                 }
             )
         )
