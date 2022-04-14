90a6fd5045a95b32807aa2ca066ef84d91b98548
Korollayre <mbuinichienko@mail.ru>
2022-04-14 10:59:41 +0300

    Add new handlers

--- microservices/book_microservice/books_backend/application/errors.py
+++ microservices/book_microservice/books_backend/application/errors.py
@@ -7,23 +7,34 @@ class FilterKeyError(AppError):
 
 
 class NoBook(AppError):
-    msg_template = "Book with id '{id}' does not exists"
+    msg_template = "Book with id '{id}' does not exists."
     code = 'book_api.no_book'
 
 
 class BookedBook(AppError):
-    msg_template = "Book with id '{book_id}' already booked"
+    msg_template = "Book with id '{book_id}' already booked."
     code = 'book_api.booked_book'
 
 
+class BoughtBook(AppError):
+    msg_template = "Sorry, but book with id '{book_id}' was bought..."
+    code = 'book_api.bought_book'
+
+
+class NoActiveBook(AppError):
+    msg_template = "Oops, you don't have booked book. " \
+                   "If you want to buy book, you need to booked it first."
+    code = 'book_api.bought_book'
+
+
 class NoBookedBook(AppError):
-    msg_template = "Book with id '{book_id}' not booked by user with id {user_id}"
+    msg_template = "Book with id '{book_id}' not booked by user with id {user_id}."
     code = 'book_api.no_booked_book'
 
 
 class BookedLimit(AppError):
     msg_template = "You already reading one book. " \
                    "If you want to read one more book, " \
-                   "you need to buy or return your previous book"
+                   "you need to buy or return your previous book."
 
     code = 'book_api.book_limit'
