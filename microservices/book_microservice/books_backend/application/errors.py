4ab93cebc86b2f378c2ae566958c0ffc3966bcfb
Korollayre <mbuinichienko@mail.ru>
2022-04-14 10:20:27 +0300

    Create new exceptions

--- microservices/book_microservice/books_backend/application/errors.py
+++ microservices/book_microservice/books_backend/application/errors.py
@@ -4,3 +4,26 @@ from evraz.classic.app.errors import AppError
 class FilterKeyError(AppError):
     msg_template = "Unexpected filter key."
     code = 'books_api.filter_key_error'
+
+
+class NoBook(AppError):
+    msg_template = "Book with id '{id}' does not exists"
+    code = 'book_api.no_book'
+
+
+class BookedBook(AppError):
+    msg_template = "Book with id '{book_id}' already booked"
+    code = 'book_api.booked_book'
+
+
+class NoBookedBook(AppError):
+    msg_template = "Book with id '{book_id}' not booked by user with id {user_id}"
+    code = 'book_api.no_booked_book'
+
+
+class BookedLimit(AppError):
+    msg_template = "You already reading one book. " \
+                   "If you want to read one more book, " \
+                   "you need to buy or return your previous book"
+
+    code = 'book_api.book_limit'
