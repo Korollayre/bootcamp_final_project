759f58386e865cf8973ba326622da1a5ca346ade
Korollayre <mbuinichienko@mail.ru>
2022-04-14 10:21:36 +0300

    Add authentication

--- microservices/book_microservice/books_backend/adapters/api/app.py
+++ microservices/book_microservice/books_backend/adapters/api/app.py
@@ -1,16 +1,29 @@
 from evraz.classic.http_api import App
+from evraz.classic.http_auth import Authenticator
 
 from books_backend.application import services
 
-from . import controllers
+from . import (
+    auth,
+    controllers,
+)
 
 
 def create_app(
         books_service: services.BooksManager,
 
 ) -> App:
+    authenticator = Authenticator(app_groups=auth.ALL_GROUPS)
+
+    authenticator.set_strategies(auth.jwt_strategy)
+
     app = App(prefix='/api')
 
-    app.register(controllers.Books(service=books_service))
+    app.register(
+        controllers.Books(
+            authenticator=authenticator,
+            service=books_service,
+        )
+    )
 
     return app
