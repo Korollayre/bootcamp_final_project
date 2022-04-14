4861e75bb8a0eaf372b78d2ade1edfe1f38f92a1
Korollayre <mbuinichienko@mail.ru>
2022-04-14 10:25:57 +0300

    Add authentication

---
+++ microservices/book_microservice/books_backend/adapters/api/auth.py
@@ -0,0 +1,25 @@
+from evraz.classic.http_auth import Group, Permission, strategies
+
+import os
+
+
+class Permissions:
+    FULL_CONTROL = Permission('full_control')
+
+
+class Groups:
+    ADMINS = Group(
+        'admins',
+        permissions=(
+            Permissions.FULL_CONTROL,
+        ),
+    )
+
+
+jwt_strategy = strategies.JWT(
+    secret_key=os.getenv('SECRET_JWT_KEY'),
+)
+
+ALL_GROUPS = (
+    Groups.ADMINS,
+)
