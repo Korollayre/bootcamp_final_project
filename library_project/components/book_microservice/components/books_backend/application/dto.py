b98a45296b5f31c78394b803ffe1668a1500e29a
Korollayre <mbuinichienko@mail.ru>
2022-04-15 10:20:39 +0300

    Add new DTO class for validate filter params

--- library_project/components/book_microservice/components/books_backend/application/dto.py
+++ library_project/components/book_microservice/components/books_backend/application/dto.py
@@ -1,7 +1,13 @@
 from datetime import datetime
-from typing import Optional
+from pydantic import validator
+from typing import (
+    Optional,
+    Tuple,
+)
 
 from evraz.classic.app import DTO
+
+from .errors import FilterKeyError
 
 
 class BooksInfo(DTO):
@@ -40,3 +46,86 @@ class BooksHistoryInfo(DTO):
     book_id: int
     user_id: int
     created_date: datetime
+
+
+class FiltersInfo(DTO):
+    title: Optional[Tuple] = None
+    authors: Optional[Tuple] = None
+    publisher: Optional[Tuple] = None
+    price: Optional[Tuple] = None
+    order_by: Optional[str] = None
+    limit: int
+    offset: int
+
+    @validator('title')
+    def title_check(cls, value: Optional[Tuple]) -> Optional[Tuple]:
+        if value is None:
+            return
+
+        field_flag, field_value = value
+
+        if field_flag not in ('like', 'eq',):
+            raise FilterKeyError(
+                field='title',
+                key=field_flag,
+            )
+
+        return 'title', field_flag, field_value
+
+    @validator('authors')
+    def authors_check(cls, value: Optional[Tuple]) -> Optional[Tuple]:
+        if value is None:
+            return
+
+        field_flag, field_value = value
+
+        if field_flag not in ('like', 'eq',):
+            raise FilterKeyError(
+                field='authors',
+                key=field_flag,
+            )
+
+        return 'authors', field_flag, field_value
+
+    @validator('publisher')
+    def publisher_check(cls, value: Optional[Tuple]) -> Optional[Tuple]:
+        if value is None:
+            return
+
+        field_flag, field_value = value
+
+        if field_flag not in ('like', 'eq',):
+            raise FilterKeyError(
+                field='publisher',
+                key=field_flag,
+            )
+
+        return 'publisher', field_flag, field_value
+
+    @validator('price')
+    def price_check(cls, value: Optional[Tuple]) -> Optional[Tuple]:
+        if value is None:
+            return
+
+        field_flag, field_value = value
+
+        if field_flag not in ('gt', 'gte', 'lt', 'lte',):
+            raise FilterKeyError(
+                field='price',
+                key=field_flag,
+            )
+
+        return 'price', field_flag, field_value
+
+    @validator('order_by')
+    def order_by_check(cls, value: Optional[str]) -> Optional[str]:
+        if value is None:
+            return
+
+        if value not in ('price', 'pages',):
+            raise FilterKeyError(
+                field=value,
+                key='order_by',
+            )
+
+        return value
