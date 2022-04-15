6818d93ec244b0a36fee84724f12faccb0191fb4
Korollayre <mbuinichienko@mail.ru>
2022-04-15 10:20:22 +0300

    Some changes in FilterKeyError handler

--- library_project/components/book_microservice/components/books_backend/application/errors.py
+++ library_project/components/book_microservice/components/books_backend/application/errors.py
@@ -2,7 +2,7 @@ from evraz.classic.app.errors import AppError
 
 
 class FilterKeyError(AppError):
-    msg_template = "Unexpected filter key."
+    msg_template = "Unexpected filter key {key} for field {field}"
     code = 'books_api.filter_key_error'
 
 
